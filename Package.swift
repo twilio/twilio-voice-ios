9779c5a4132c0e2567b85efb7cb69ca8aba6d43e
bobiechen-twilio <bchen@twilio.com>
2021-02-01 12:40:44 -0800

    Fix 6.2.0 artifact URL (#67)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVoice",
-            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.0/TwilioVoice.framework.zip",
+            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.0/TwilioVoice.xcframework.zip",
             checksum: "d5612adde27bbccc95c05891d901836fe0760972955413df492aa025909d74e7"
         )
     ]
