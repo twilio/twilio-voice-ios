0fa4ca2b87ade9bb67bc917df47fa893ddcb49c8
twilio-sdk-build <twilio-sdk-build@twilio.com>
2021-03-11 19:01:17 +0000

    Update Package.swift for 6.2.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVoice",
-            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.1/TwilioVoice.xcframework.zip",
-            checksum: "fc69a441a3f19e772bdd60dfcf9203cb6fc00b9a1123eabe3a524d60e3820dbb"
+            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.2/TwilioVoice.xcframework.zip",
+            checksum: "e8411732eff2ad79bcba744385b402523f2bddc9cb9c8654faa27b62dc9a12ec"
         )
     ]
 )
