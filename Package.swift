1eb7c2d4d87b694e74c50c60885271109342d8e9
Bobie Chen <bchen@twilio.com>
2026-03-20 10:45:26 -0700

    Revert "Update for 0.9.9"
    This reverts commit 88203367a7a44d50d1e58a457d85abfb5eb68a56.

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVoice",
-            url: "https://github.com/twilio/twilio-voice-ios/releases/download/0.9.9/TwilioVoice.xcframework.zip",
-            checksum: "21deb16c46ed2f432917352860b4248e2d9e60fcf08652cef410406c8d0b33b1"
+            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.13.6/TwilioVoice.xcframework.zip",
+            checksum: "4e04fa2698e33a47d15293f4437b1416fccad29c6e168695295c090c661d8acb"
         ),
         .binaryTarget(
             name: "TwilioVoice-static",
-            url: "https://github.com/twilio/twilio-voice-ios/releases/download/0.9.9/TwilioVoice-static.xcframework.zip",
-            checksum: "920249db4a7f9bfe1cbc59f3f7d4f278988091a4ae37540cf92eafda090353c6"
+            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.13.6/TwilioVoice-static.xcframework.zip",
+            checksum: "cf318d7590fcd4dfbe52be5fc34cbc9da3f8661e0d9a93714c51a379fee1dc8e"
         )
     ]
 )
